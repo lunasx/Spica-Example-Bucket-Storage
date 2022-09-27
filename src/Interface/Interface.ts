aa20b523f1f1417d6917c6ff4f89115d2f297be0
luna <cagdasakt6@gmail.com>
2022-09-27 16:11:38 +0300

    first commit

---
+++ src/Interface/Interface.ts
@@ -0,0 +1,5 @@
+export interface IData {
+    _id: number,
+    title: string,
+    desc: string
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import * as Bucket from '@spica-devkit/bucket';
+import * as Storage from '@spica-devkit/storage';
+import { IData } from 'src/Interface/Interface';
+
+Bucket.initialize({
+  publicUrl: 'https://intership-test-1ae96.hq.spicaengine.com/api',
+  identity:
+    '< identity key >',
+});
+
+Bucket.initialize({
+  publicUrl: 'https://intership-test-1ae96.hq.spicaengine.com/api',
+  apikey:
+    '< api key >',
+});
+
+Storage.initialize({
+  publicUrl: 'https://intership-test-1ae96.hq.spicaengine.com/api',
+  identity:
+    '< identity key >',
+});
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'spica';
+  buckets: IData[] = [];
+  storageData = this.getStorage();
+
+  async ngOnInit() {
+    this.bucketPromise()
+      .then((data) => {
+        localStorage.setItem('buckets', JSON.stringify(data));
+        const data_obj = localStorage.getItem('buckets');
+        this.buckets = JSON.parse(data_obj || '');
+      })
+      .catch((error) => {
+        console.log(JSON.stringify(error));
+      });
+
+    this.getStorage()
+      .then((data) => {
+        console.log(JSON.stringify(data));
+      })
+      .catch((error) => {
+        console.log(JSON.stringify(error));
+      });
+  }
+
+  bucketPromise() {
+    return Bucket.data.getAll('< bucket id >');
+  }
+
+  getStorage() {
+    return Storage.getAll();
+  }
 }
